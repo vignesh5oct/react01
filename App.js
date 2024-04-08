8dab3096dee23dad4cb390b75d6daf47816f4b51
vignesh5oct <vignesh5oct@gmail.com>
2024-04-08 20:49:37 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const headingReact = React.createElement("div", {id : "parent"}, 
+    React.createElement("div",{ id : "child"},
+    [React.createElement("h1",{},"H1 Tag"),
+    React.createElement("h2",{},"H2 tag")]),
+
+    React.createElement("div", {id : "child2"}, 
+    [React.createElement("h1",{},"H1 Tag"),
+    React.createElement("h2",{},"H2 tag")]),
+    );
+
+
+const rootReact = ReactDOM.createRoot(document.getElementById("root"));
+rootReact.render(headingReact);
+
