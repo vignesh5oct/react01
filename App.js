55a69d79b1fd731b358a3529b2e04e1e197efb56
vignesh5oct <vignesh5oct@gmail.com>
2024-04-09 12:49:35 +0530

    episode 02 completed

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const headingReact = React.createElement("div", {id : "parent"}, 
     React.createElement("div",{ id : "child"},
     [React.createElement("h1",{},"H1 Tag"),
